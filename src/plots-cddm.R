59cf8d842af6d83526a7fb0db2b0a2eabca078df
ManuelVU <jesus_mvu@hotmail.com>
2022-10-28 16:12:59 -0700

    addingangle and response time plots

--- src/plots-cddm.R
+++ src/plots-cddm.R
@@ -1,7 +1,8 @@
 # Load libraries and data -------------------------------------------------
 library(tidyverse)
+library(truncnorm)
 
-read_csv(file = "data/orientation/orientation.csv")
+orientation <- read_csv(file = "data/orientation/orientation.csv")
 samples <- readRDS("data/posteriors/posterior-test-eta-omega-cddm.RDS")
 
 # Figure one: tested parameter values -------------------------------------
@@ -212,17 +213,26 @@ dev.off()
 
 
 # Figure two: predictive distribution by participant ----------------------
+center <- 90
 spacing <- 10
-space <- 180
-centers <- c(space/2, 
-             3 /2 * (space + spacing), 
-             2 * space + (space + spacing)/2)
+dimention <- 180
+centers <- c(center + 0 * spacing + 0 * dimention,
+             center + 1 * spacing + 1 * dimention,
+             center + 2 * spacing + 2 * dimention)
 
 sd_target <- 180*sqrt(samples$BUGSoutput$sims.list$var_pos/4)/pi
 
+pdf(file = "fig/response-angle-participant-dif.pdf")
+layout(matrix(seq(1,dim(sd_target)[2]), byrow = TRUE, ncol = 3))
+par(oma = c(3,3,1,1),
+    mai = c(0.2,0.2,0.1,0.1),
+    yaxs = "i")
+
 for(ii in 1:dim(sd_target)[2]){
-  plot(x = 0, y = 0, xlim = c(0, 3 * space + 2 * spacing), 
-       ylim = c(0,0.04), ann = FALSE, axes = FALSE, type = "n")
+  plot(x = 0, y = 0, xlim = c(0, 3 * dimention + 2 * spacing), 
+       ylim = c(0,0.1), ann = FALSE, axes = FALSE, type = "n")
+  
+  abline(v = centers[1:2] + center + spacing/2, lwd = 1.3, col = "#36454f")
   
   for(dd in 1:dim(sd_target)[3]){
     h1 <- hist( 180 / pi * orientation$difference[orientation$id == ii & 
@@ -236,12 +246,151 @@ for(ii in 1:dim(sd_target)[2]){
                       each = 2),
                 rev(rep(h1$breaks[which(h1$density > 0)][-1], each = 2)),
                 h1$breaks[which(h1$density > 0)][1]),
-            y = c(rep(h1$density[which(h1$density>0)],
-                        each = 2),
+            y = c(rep(h1$density[which(h1$density>0)],each = 2),
                   rep(x = 0, times = length(rep(h1$density[which(h1$density>0)],
                       each = 2)))),
-            border = FALSE, col = difficulty_col[dd])
+            border = "black", col = difficulty_col[dd])
+    
+    # curve(dnorm(x = x, mean = centers[dd], 
+    #             sd = quantile(x = sd_target[,ii,dd], prob = 0.5)), 
+    #       from = centers[dd] - 90, to = centers[dd] + 90, add = T, 
+    #       col = difficulty_col[dd], lwd = 3)
+    curve(dnorm(x = x, mean = centers[dd], sd = mean(x = sd_target[,ii,dd])), 
+          from = centers[dd] - 90, to = centers[dd] + 90, add = T, 
+          col = difficulty_col[dd], lwd = 3)
+    
   }
   axis(side = 1, at = c(centers - 90, centers, centers + 90), 
        labels = c(rep(c("-90", "0", "90"), each = 3)))
+  box()
 }
+dev.off()
+# Figure three: predictive distribution by deflection ---------------------
+center <- 0
+spacing <- 5
+dimention <- 90
+centers <- c(center + 0 * spacing + 0 * dimention,
+             center + 1 * spacing + 1 * dimention,
+             center + 2 * spacing + 2 * dimention,
+             center + 3 * spacing + 3 * dimention)
+
+floors <- c(0.2, 0.1, 0)
+curve_color <- "#221d1c"
+
+sd_target <- 180*sqrt(samples$BUGSoutput$sims.list$var_pos/4)/pi
+omega <- 1 - samples$BUGSoutput$sims.list$omega[,,c(1,2,3,5,6,7)]
+sd_cue <- 180*sqrt(samples$BUGSoutput$sims.list$var_cue/4)/pi
+positions <- rbind(c(0,0),
+                   c(3,4),
+                   c(2,5),
+                   c(1,6))
+abs_def <- c(0,20,50,70)
+
+pdf(file = "fig/response-angle-participant-dif-cue.pdf")
+layout(matrix(seq(1,dim(sd_target)[2]), byrow = TRUE, ncol = 3))
+par(oma = c(3,3,1,1),
+    mai = c(0.2,0.2,0.1,0.1),
+    yaxs = "i")
+
+for(ii in 1:dim(sd_target)[2]){
+  plot(x = 0, y = 0, xlim = c(0, 4 * dimention + 3 * spacing),
+       ylim = c(0, 3 * 0.095), ann = FALSE, axes = FALSE, type = "n")
+
+  abline(v = centers[2:4] - spacing/2, lwd = 1.3)
+  abline(h = floors[1:2], lwd = 1.3)
+  
+  for(dd in 1:dim(sd_target)[3]){
+    for(aa in 1:dim(sd_cue)[2]){
+      h1 <- hist( 180 / pi * abs(orientation$difference[orientation$id == ii & 
+                                                    orientation$difficulty_id == dd &
+                                                    orientation$absolute_cue_id == aa]), 
+                  breaks = seq(0, 90, length.out = 18), plot = FALSE)
+      
+      polygon(x = centers[aa] +
+                c(h1$breaks[which(h1$density > 0)][1],
+                  rep(c(h1$breaks[which(h1$density > 0)][-1],
+                        h1$breaks[tail(x = which(h1$density > 0), n = 1) + 1]),
+                      each = 2),
+                  rev(rep(h1$breaks[which(h1$density > 0)][-1], each = 2)),
+                  h1$breaks[which(h1$density > 0)][1]),
+              y = c(floors[dd]+rep(h1$density[which(h1$density>0)],each = 2),
+                    rep(x = floors[dd], times = length(rep(h1$density[which(h1$density>0)],
+                                                  each = 2)))),
+              border = "black", col = difficulty_col[dd])
+      if(aa>1){
+        curve(floors[dd] + mean(omega[,ii,positions[aa,]])*
+                dtruncnorm(x = x,a = centers[aa], b = centers[aa] + 90, 
+                           mean = centers[aa], 
+                           sd = mean(x = sd_target[,ii,dd]))+
+              (1-mean(omega[,ii,positions[aa,]])) *                 
+                dtruncnorm(x = x, a = centers[aa], b = centers[aa] + 90, 
+                           mean = centers[aa] + abs_def[aa], 
+                           sd = mean(x = sd_cue[,aa])), 
+              from = centers[aa], to = centers[aa] + 90, add = T, 
+              col = curve_color, lwd = 1.6, lty = 1)
+      }
+      else{
+        curve(floors[dd] + 
+                dtruncnorm(x = x,a = centers[aa], b = centers[aa]+90, 
+                           mean = centers[aa], 
+                           sd = mean(x = sd_target[,ii,dd])), 
+              from = centers[aa], to = centers[aa] + 90, add = T, 
+              col = curve_color, lwd = 1.6, lty = 1)
+      }
+    }
+  }
+  axis(side = 1, at = c(centers, centers + 90),
+       labels = c(rep(c("0", "90"), each = 4)))
+  box()
+}
+dev.off()
+
+# Figure four: response time distributions --------------------------------
+center <- 0
+spacing <- 1
+dimention <- 2.5
+centers <- c(center + 0 * spacing + 0 * dimention,
+             center + 1 * spacing + 1 * dimention,
+             center + 2 * spacing + 2 * dimention,
+             center + 3 * spacing + 3 * dimention)
+
+floors <- c(11, 5.5, 0)
+
+pdf(file = "fig/response-time-participant-dif-cue.pdf")
+layout(matrix(seq(1,length(unique(orientation$id))), byrow = TRUE, ncol = 3))
+par(oma = c(3,3,1,1),
+    mai = c(0.2,0.2,0.1,0.1),
+    yaxs = "i")
+
+for(ii in 1:length(unique(orientation$id))){
+  plot(x = 0, y = 0, xlim = c(0, 4 * dimention + 3 * spacing),
+       ylim = c(0, floors[1] + 5.5), ann = FALSE, axes = FALSE, type = "n")
+
+  abline(v = centers[2:4] - spacing/2, lwd = 1.3)
+  abline(h = floors[1:2], lwd = 1.3)
+  
+  for(dd in 1:dim(sd_target)[3]){
+    for(aa in 1:dim(sd_cue)[2]){
+      h1 <- hist(orientation$response_time[orientation$id == ii & 
+                                           orientation$difficulty_id == dd &
+                                           orientation$absolute_cue_id == aa], 
+                 plot = FALSE, breaks = seq(0,2.5,length.out = 16))
+      
+      polygon(x = centers[aa] +
+                c(h1$breaks[which(h1$density > 0)][1],
+                  rep(c(h1$breaks[which(h1$density > 0)][-1],
+                        h1$breaks[tail(x = which(h1$density > 0), n = 1) + 1]),
+                      each = 2),
+                  rev(rep(h1$breaks[which(h1$density > 0)][-1], each = 2)),
+                  h1$breaks[which(h1$density > 0)][1]),
+              y = c(floors[dd]+rep(h1$density[which(h1$density>0)],each = 2),
+                    rep(x = floors[dd], times = length(rep(h1$density[which(h1$density>0)],
+                                                           each = 2)))),
+              border = "black", col = difficulty_col[dd])
+    }
+  }
+  axis(side = 1, at = c(centers, centers + 1, centers + 2),
+       labels = c(rep(c("0", "1", "2"), each = 4)))
+  box()
+}
+dev.off()
